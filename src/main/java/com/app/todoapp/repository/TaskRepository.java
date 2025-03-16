d561014c8246163739874d206c2991c1de789f53
pranav9gupta <pranav09261630@gmail.com>
2025-03-16 19:58:41 +0530

    second commit

--- src/main/java/com/app/todoapp/repository/TaskRepository.java
+++ src/main/java/com/app/todoapp/repository/TaskRepository.java
@@ -1,4 +1,7 @@
 package com.app.todoapp.repository;
 
-public interface TaskRepository {
+import org.springframework.data.jpa.repository.JpaRepository;
+import com.app.todoapp.models.Task;
+public interface TaskRepository extends JpaRepository<Task, Long> {
+    // this is an interface that extends JpaRepository interface from Spring Data JPA library created to handle the database operations like create, read, update, delete (CRUD) operations. it extends using Entity name, and primary key type
 }
